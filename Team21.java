b9a8efdaa25a1445cae0e880132af83388fefa44
yanyanll2l <prapra5621@gmail.com>
2025-04-16 21:56:59 +0800

    add member
    Hi~

--- Team21.java
+++ Team21.java
@@ -2,5 +2,6 @@ public class Team21{
     public static void main(String[] args) {
         System.out.println("Put your name in this");
         System.out.println("Student ID : 01257127 Name: 林哲緯");
+        System.out.println("Student ID : 01131010 Name: 周君諺");
     }
 }
