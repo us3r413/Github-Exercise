e1fd4d2d8471cb502884983c49cc7b20eabc8184
Someone <linzhewei123@gmail.com>
2025-04-16 16:11:12 +0800

    Create Team21.java

---
+++ Team21.java
@@ -0,0 +1,6 @@
+public class Team21{
+    public static void main(String[] args) {
+        System.out.println("Put your name in this");
+        System.out.println("Student ID : 01257127 Name: 林哲緯");
+    }
+}
