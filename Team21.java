150fca8d9efedc4496ad57be3a652eb9a2e46fc5
SeanShiue2005 <151007291+SeanShiue2005@users.noreply.github.com>
2025-04-17 13:36:09 +0800

    add student

--- Team21.java
+++ Team21.java
@@ -3,5 +3,6 @@ public class Team21{
         System.out.println("Put your name in this");
         System.out.println("Student ID : 01257127 Name: 林哲緯");
         System.out.println("Student ID : 01131010 Name: 周君諺");
+        System.out.println("Student ID : 01257155 Name: 薛翔安");
     }
 }
